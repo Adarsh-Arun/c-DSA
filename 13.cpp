bf17b77e1ee91911784373a21a3754fb37547574
Adarsh arun <fuud47@gmail.com>
2024-10-19 15:42:39 +0530

    Add files via upload

---
+++ 13.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+# include <math.h>
+#include <string>
+using namespace std;
+
+int main (){
+    std::cout << "Hey guys!"<<"\n";
+    std::cout << "Myself new coder in the community"<<std::endl;
+    std::cout << "This journey will be very exiting"<< std::endl;
+    return 0;
+}
