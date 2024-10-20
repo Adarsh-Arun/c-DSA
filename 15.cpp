7e49d579113325da3b9d5a4e84e92b9bcae895b9
Adarsh arun <fuud47@gmail.com>
2024-10-21 00:48:40 +0530

    Add files via upload

---
+++ 15.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cout << "Enter the value of x :" ;
+    cin>> x;
+    cout << "The value of x is :" <<x <<"\n";
+    int y ;
+    cout << "Enter the value of y : ";
+    cin>> y;
+    cout << "The value of x is :"<<x<< " and the value of y is :"<< y;
+    
+    
+    return 0;
+}
