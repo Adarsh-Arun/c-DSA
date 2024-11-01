0e8bc84ff6ff32bc7e19ee4db77993d3dabe35b0
Adarsh arun <fuud47@gmail.com>
2024-11-01 18:33:47 +0530

    Add files via upload

---
+++ 22.cpp
@@ -0,0 +1,26 @@
+// for loops 
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+//for
+int main (){
+    for (int i=1; i<=5; i++){
+        
+        cout <<" hey "<< endl;
+    }
+    
+    
+//while
+    int j=1;
+    while (j<=10){
+        cout << "horsh"<< endl;
+        
+        j++;
+    }
+    
+    
+    return 0;
+}
